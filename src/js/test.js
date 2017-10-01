b55727c069008b46e1a4bc97c132da525c38b125
Piotr Wolski <piotr.wolski@yahoo.co.uk>
2017-10-01 23:27:24 +0100

    Exercise #3

--- src/js/test.js
+++ src/js/test.js
@@ -5,7 +5,6 @@ $(document).ready(function () {
     $(window).resize(function () {
         setContentHeight();
     }).resize();
-
     setContentHeight();
 });
 
@@ -14,4 +13,39 @@ setContentHeight = function () {
     var height = $(this).height() - $('header').outerHeight() - $('.main-nav-outer').outerHeight() - $('footer').outerHeight();
     $(".main-section").css('max-height', height + 'px');
     $(".sidebar").height(height - height * 0.05);
+};
+
+
+// Toggle Sidebar
+toggleSidebar = function () {
+    var sidebarVisible = $('.sidebar').width() > 0;
+
+    if (sidebarVisible) {
+        $('.sidebar').width(0);
+        $('.content').width('100vw');
+        return;
+    }
+    $('.sidebar').width('30vw');
+    $('.content').width('70vw');
+};
+
+// Toggle Sidebar test
+
+toggleSidebarTest = function () {
+
+    var sidebarVisible = $('.sidebar').width() > 0;
+
+    $('#btnToggleSidebar').trigger('click', null);
+
+    setTimeout(function () {
+        if ((sidebarVisible && $('.sidebar').width() == 0)
+            || (!sidebarVisible && $('.sidebar').width() > 0)) {
+            console.info("TEST PASS: Toggle button toggles sidebar...");
+        }
+        else {
+            console.error("TEST FAILED: Toggle button toggles sidebar...");
+        }
+        $('#btnToggleSidebar').trigger('click', null);
+    }, 1000);
+
 };
